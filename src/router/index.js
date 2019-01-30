79aa114a233dcceb4c73ba7636da3ca6317481c4
2924281806 <39575974+2924281806@users.noreply.github.com>
2019-01-30 16:26:05 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/home/home.vue'
-
+import Home from '@/components/home/Home'
+import Carte from '@/components/carte/Carte'
+import Order from '@/components/order/Order'
+import Cart from '@/components/cart/Cart'
+import Mine from '@/components/mine/Mine'
+import Addr from '@/components/addr/Addr'
+import City from '@/components/city/City'
 
 Vue.use(Router)
-
 
 export default new Router({
   routes: [
@@ -12,6 +16,30 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    }, {
+      path: '/Carte',
+      name: 'Carte',
+      component: Carte
+    }, {
+      path: '/Order',
+      name: 'Order',
+      component: Order
+    }, {
+      path: '/Cart',
+      name: 'Cart',
+      component: Cart
+    }, {
+      path: '/Mine',
+      name: 'Mine',
+      component: Mine
+    }, {
+      path: '/Addr',
+      name: 'Addr',
+      component: Addr
+    }, {
+      path: '/City',
+      name: 'City',
+      component: City
     }
   ]
 })
