c90ac214324a88acc2d895bf05983dcaf5710ad7
2924281806 <39575974+2924281806@users.noreply.github.com>
2019-01-30 16:32:50 +0800

    Create Cart.vue

---
+++ src/components/cart/Cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="cart">
+    <home-footer></home-footer>
+  </div>
+</template>
+
+<script>
+import HomeFooter from '../common/Footer.vue'
+export default {
+  name: 'cart',
+  components: {
+    'home-footer': HomeFooter
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
