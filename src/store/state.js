629eb7f66ba1df603d111f87dabfc1273440e2eb
2924281806 <39575974+2924281806@users.noreply.github.com>
2019-01-30 16:30:29 +0800

    Create state.js

---
+++ src/store/state.js
@@ -0,0 +1,14 @@
+/**
+ * Created by cWX585857 on 2018/9/18.
+ */
+let defaultCity = '深圳'
+try{
+  if(localStorage.city){
+    defaultCity = localStorage.city
+  }
+}catch (e){}
+
+
+export default{
+  city: defaultCity
+}
