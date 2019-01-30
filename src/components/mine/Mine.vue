b4b43127b47bfd4769110442c9e7876c1a5b7f10
2924281806 <39575974+2924281806@users.noreply.github.com>
2019-01-30 16:34:17 +0800

    Create Mine.vue

---
+++ src/components/mine/Mine.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mine">
+    <home-footer></home-footer>
+  </div>
+</template>
+
+<script>
+import HomeFooter from '../common/Footer.vue'
+export default {
+  name: 'mine',
+  components: {
+    'home-footer': HomeFooter
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
