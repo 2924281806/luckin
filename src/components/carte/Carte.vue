9be3274c81269100148195c74c22c04fbeeee01b
2924281806 <39575974+2924281806@users.noreply.github.com>
2019-01-30 16:33:16 +0800

    Create Carte.vue

---
+++ src/components/carte/Carte.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="carte">
+      <menu-carousel></menu-carousel>
+      <menu-goods :goods="goods"></menu-goods>
+      <home-footer></home-footer>
+    </div>
+</template>
+
+<script>
+import MenuCarousel from './page/Carousel.vue'
+import HomeGoods from './page/Goods.vue'
+import HomeFooter from '../common/Footer.vue'
+import axios from 'axios'
+export default {
+  name: 'carte',
+  data () {
+    return {
+      goods: []
+    }
+  },
+  components: {
+    'menu-carousel': MenuCarousel,
+    'menu-goods': HomeGoods,
+    'home-footer': HomeFooter
+  },
+  methods: {
+    getGoodsInfo () {
+      axios.get('api/goods.json').then(this.getGoodsInfoSucc)
+    },
+    getGoodsInfoSucc (res) {
+      const data = res.data
+      this.goods = data.goods
+      console.log(this.goods)
+    }
+  },
+  mounted () {
+    this.getGoodsInfo()
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
