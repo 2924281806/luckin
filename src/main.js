00dcbe59dca778329cd08b064dd19c659e25e4c8
2924281806 <39575974+2924281806@users.noreply.github.com>
2019-01-30 16:29:16 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import '@/assets/styles/reset.css'
+import fastClick from 'fastclick'
+import store from './store/index'
+import 'styles/reset.css'
+import 'styles/iconfont.css'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+// require styles
 import 'swiper/dist/css/swiper.css'
 
-Vue.config.productionTip = false
 Vue.use(VueAwesomeSwiper)
-
+fastClick.attach(document.body)
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  render: h => h(App)
 })
