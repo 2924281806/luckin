16766423b1f7a45cbaa0160b7a40458c1c40ecb3
2924281806 <39575974+2924281806@users.noreply.github.com>
2019-01-30 16:34:37 +0800

    Create Order.vue

---
+++ src/components/order/Order.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="order">
+    <home-footer></home-footer>
+  </div>
+</template>
+
+<script>
+import HomeFooter from '../common/Footer.vue'
+export default {
+  name: 'order',
+  components: {
+    'home-footer': HomeFooter
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
